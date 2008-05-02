87e1a232e93ed679a24c23dae89c053b4ed31f17
dbader <mail@dbader.org>
2008-05-02 13:00:33 +0000

    Everything works again.

--- run_bochs.sh
+++ run_bochs.sh
@@ -5,5 +5,5 @@
 # This loads a bochsrc from the current directory.
 
 sudo /sbin/losetup /dev/loop0 floppy.img
-sudo bochs -f tools/bochsrc
+sudo bochs -f src/tools/bochsrc
 sudo /sbin/losetup -d /dev/loop0
