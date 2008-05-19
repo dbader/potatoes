1541af02daa79a2baf62c258c3c5dc80f02845e0
dtraytel <dmitriy.traytel@googlemail.com>
2008-05-19 10:25:01 +0000

    added svn properties to limits.h

--- src/kernel/include/limits.h
+++ src/kernel/include/limits.h
@@ -1,4 +1,4 @@
-/* $Id: const.h 68 2008-05-17 22:16:52Z dtraytel $
+/* $Id$
       _   _  ____   _____ 
      | | (_)/ __ \ / ____|
   ___| |_ _| |  | | (___  
@@ -25,8 +25,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * Definition of the maximum and minimum values of different types.
  *
  * @author Dmitriy Traytel
- * @author $LastChangedBy: dtraytel $
- * @version $Rev: 68 $
+ * @author $LastChangedBy$
+ * @version $Rev$
  */
 
 #define SINT8_MAX       0x7F
